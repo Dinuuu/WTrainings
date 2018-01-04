cda82e0190dff08606fb4884f43c01ff0ce769bd
“Federico <federico.dinucci@wolox.com.ar>
2018-01-03 22:34:31 -0300

    remove default month

--- app/controllers/api/v1/charts_controller.rb
+++ app/controllers/api/v1/charts_controller.rb
@@ -1,16 +1,16 @@
 module Api
   module V1
     class ChartsController < ApiController
-      before_action :set_default_month
 
       def trainings_per_month
-        render json: TrainingSessionQuery.new.find(filter_params).group_by_month(:dictation_date, format: "%b %Y").count
+        render json: TrainingSessionQuery.new.find(filter_params)
+                      .group_by_month(:dictation_date, format: "%b %Y").count
       end
 
       def trainings_per_type
         render json: TrainingSessionQuery.new.find(filter_params)
                       .joins(:training)
-                      .group('trainings.kind').count.map { |kind, count| { Training.kinds.key(kind) => count } }
+                      .group('trainings.kind').count.transform_keys! { |kind| Training.kinds.key(kind) }
       end
 
       def monthly_info
@@ -18,10 +18,6 @@ module Api
       end
 
       private
-      
-      def set_default_month
-        params[:month] ||= Time.zone.now.strftime("%B")
-      end
 
       def filter_params
         params.permit(:month, :year)
