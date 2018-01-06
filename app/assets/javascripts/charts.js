8df4d8e4c8440f23e5f9bb0ca4dd2857f0daccf2
“Federico <federico.dinucci@wolox.com.ar>
2018-01-06 16:46:05 -0300

    Pass parameters to InfoSummary

--- app/assets/javascripts/charts.js
+++ app/assets/javascripts/charts.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-$.ajax({ url: "api/v1/charts/summary_info",
+$.ajax({ url: "api/v1/charts/summary_info" + window.location.search,
         context: document.body,
         success: function(response){
         	document.getElementById('woloxers-trained').innerHTML=response.woloxers_trained;
